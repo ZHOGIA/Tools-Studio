84ba705ecfad354bdab467e200ab8b297ed637d7
ZHOGIA <zhogiachmad@gmail.com>
2026-03-31 06:19:21 +0700

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+    enabled: false // Menyembunyikan panel Astro di pojok kiri bawah
+  },
+  server: {
+    port: 3000, // Anda bisa mengubah angka ini sesuai dengan port yang Anda inginkan
+  },
+  vite: {
+    server: {
+      allowedHosts: true, // Mengizinkan diakses lewat domain public (Cloudflare/Ngrok)
+    }
+  }
+});
